47c9315b64fc4151955528109dd701bcc4a179e1
Tritium Labs <114547301+TritiumLabs@users.noreply.github.com>
2023-10-02 10:19:50 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 var el = document.getElementById("clickMe");
 if (el.addEventListener)
-    el.addEventListener("click", doFunction, false);
+    el.addEventListener("click", openInNewTab, false);
 else if (el.attachEvent)
-    el.attachEvent('onclick', doFunction);
+    el.attachEvent('onclick', openInNewTab);
 
 function openInNewTab(url) {
   window.open(url, '_blank').focus();
